dbeabbb7f4e1530a65a335ff778ca8b6f114bf8e
Brandon Chavarría López <59859071+Brandchal@users.noreply.github.com>
2021-04-05 19:15:44 -0600

    Update home.js
    Update Navbar

--- src/js/component/home.js
+++ src/js/component/home.js
@@ -1,10 +1,10 @@
 import React from "react";
 
-//include images into your bundle
+//includes images into your bundle
 import { NavBar } from "./navbar.js";
 import { Cards } from "./cards.js";
 
-//create your first component
+//creates your first component
 export function Home() {
 	return (
 		<div>
